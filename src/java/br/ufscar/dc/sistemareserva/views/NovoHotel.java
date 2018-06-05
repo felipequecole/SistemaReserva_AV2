c93aff99daac891be1b62d1161c41f206af49962
FranksonTeotonho <frankson.teotonho@gmail.com>
2018-06-05 18:45:10 -0300

    Forms hotel funcionando

--- src/java/br/ufscar/dc/sistemareserva/views/NovoHotel.java
+++ src/java/br/ufscar/dc/sistemareserva/views/NovoHotel.java
@@ -8,22 +8,48 @@ package br.ufscar.dc.sistemareserva.views;
 import br.ufscar.dc.sistemareserva.beans.Hotel;
 import br.ufscar.dc.sistemareserva.dao.HotelDAO;
 import java.io.Serializable;
+import java.sql.SQLException;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.naming.NamingException;
 
 /**
  *
  * @author felipequecole
  */
-
 @Named
 @SessionScoped
 public class NovoHotel implements Serializable {
-    
-    @Inject HotelDAO hoteldao; 
+
+    @Inject
+    HotelDAO hotelDao;
     Hotel dadosHotel;
-    
+
+    public NovoHotel() {
+        dadosHotel = new Hotel();
+    }
+
+    public Hotel getDadosHotel() {
+        return dadosHotel;
+    }
+
+    public void setDadosHotel(Hotel dadosHotel) {
+        this.dadosHotel = dadosHotel;
+    }
+
+    public String gravarHotel() throws SQLException, NamingException {
+
+        Hotel h = hotelDao.gravaHotel(dadosHotel);
         
-    
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        Flash flash = facesContext.getExternalContext().getFlash();
+        flash.setKeepMessages(true);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Hotel cadastrado"));
+        
+        return "index?faces-redirect=true";
+    }
 }
