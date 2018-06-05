9863b08610ca3fc916d8ea1a9c1d5316f3d4852d
FranksonTeotonho <frankson.teotonho@gmail.com>
2018-06-05 20:34:21 -0300

    Listar hoteis

--- src/java/br/ufscar/dc/sistemareserva/views/ListaHotel.java
+++ src/java/br/ufscar/dc/sistemareserva/views/ListaHotel.java
@@ -5,10 +5,38 @@
  */
 package br.ufscar.dc.sistemareserva.views;
 
+import br.ufscar.dc.sistemareserva.beans.Hotel;
+import br.ufscar.dc.sistemareserva.dao.HotelDAO;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.naming.NamingException;
+
 /**
  *
  * @author frankson
  */
-public class ListaHotel {
-    
+@Named
+@RequestScoped
+public class ListaHotel implements Serializable {
+
+    @Inject
+    HotelDAO hotelDao;
+    List<Hotel> listaHotel;
+
+    public List<Hotel> getListaHotel() {
+        return listaHotel;
+    }
+
+    public void setListaHotel(List<Hotel> listaHotel) {
+        this.listaHotel = listaHotel;
+    }
+
+    public String verTodosHoteis() throws SQLException, NamingException {
+        listaHotel = hotelDao.listaTodosHoteis();
+        return "listaHotel";
+    }
 }
